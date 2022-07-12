6843c086450afbcf2ff41c39b8a1a78e0e8cb75b
Javier Daza <javierdaza@iMac-de-Javier.local>
2022-07-12 10:18:04 -0500

    [cookiecutter-streamlit] create template

---
+++ {{cookiecutter.repo_name}}/pages/1_.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+st.set_page_config(page_title='',layout='wide',page_icon="./images/favicon.ico")#,initial_sidebar_state="collapsed")
+hide_streamlit_style = """<style>#MainMenu {visibility: hidden;}footer {visibility: hidden;}</style>"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+st.title('')
